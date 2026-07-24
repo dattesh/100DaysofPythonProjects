653b707cc22a74d23df63afb130f2b592bdb857d
Datteshkummar Gurjar <er.dattesh@gmail.com>
2026-07-24 23:54:46 +0530

    DAy 11 added

---
+++ Day11- CAPSTONE PROJECT/README.md
@@ -0,0 +1,28 @@
+A Capstone Project - Blackjack Card Game
+Rules :- Card 2 has Value 2
+        Card 3 has Value 3
+        Card 4 has Value 4
+        Card 5 has Value 5
+        Card 6 has Value 6
+        Card 7 has Value 7
+        Card 8 has Value 8
+        Card 9 has Value 9
+        Card 10 has Value 10
+        Card jack has Value 10
+        Card Queen has Value 10
+        Card King has Value 10
+        Card ACE has value either 1 or 11
+
+Rule 1: While share card, First two cards to share with USer and 
+        Computer randomly. abd its total is 21   return 0
+Rule 2: If Ace in the card and total is greater than 21 Consider ACE's Value as 1
+Rule 3: If the  USer or Computer has a black jack od if USer's score is 
+         greater than 21 then the game over
+Rule 4: If user Doesn't want to draw and want to pick another cards .Cards should be add in user's card . If user says no Game end
+Rule 5: After Finish USer's Turn , computer will select the card. Until score is 17
+Rule 6: Compare the Score 
+         If Both has same score : Draw
+         If Computer has Black jack ,Computer wins and vice versa
+         if the user's score is over 21 , Computer wins and vice versa
+         if none of the above Hoghest score wins 
+
---
+++ Day11- CAPSTONE PROJECT/black_jack.py
@@ -0,0 +1,79 @@
+import random
+
+
+def deal_card():
+    """Returns a random card from the deck """
+    cards =[11,2,3,4,5,6,7,8,9,10,10,10,10]
+    card =random.choice(cards)
+    return card
+
+def calculate_score(cards):
+    """Take a list of cards and return the score calculate
+    from the cards"""
+    if sum(cards) == 21 and len(cards) ==2:
+        return 0
+    if 11 in cards and sum(cards)>21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
+
+def compare_score(u_score, c_score):
+    if u_score == c_score:
+        return 'Draw'
+    elif c_score == 0 :
+        return "Computer has a Blck jack . You loss"
+    elif u_score == 0:
+        return "You have a Black jack .You win"
+    elif u_score >21:
+        return "Computer wins !"
+    elif c_score >21:
+        return "You win!"
+    elif u_score > c_score:
+        return "You win"
+    else :
+        return "Computer win"
+
+def play_game():
+    user_cards =[]
+    computer_cards=[]
+    is_game_over=False
+    computer_score =-1
+    user_score =-1
+
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+
+        print(f"your cards: {user_cards},Current_score: {user_score} ")
+        print(f"Computer's first card: {computer_cards[0]}")
+
+        if user_score == 0 or computer_score ==0 or user_score>21:
+            is_game_over=True
+        else:
+            user_should_deal=input("Type Y to get   another card .Type N to pass :").upper()
+            if user_should_deal == 'Y':
+                user_cards.append(deal_card())
+            else :
+                is_game_over = True
+
+    while computer_score !=0 and computer_score <17:
+        computer_cards.append(deal_card())
+        computer_score =calculate_score(computer_cards)
+    print(f'your final hands {user_cards} your final score {user_score}')
+    print(f'Computer final hands {computer_cards} Computer final score {computer_score}')
+    print(compare_score(user_score,computer_score))
+
+
+#lay_again =input("You want to play again Type Y . or Type N :").upper()
+while input("You want to play again Type Y . or Type N :").upper() == 'Y':
+    play_game()
+
+
+
+
+
+
